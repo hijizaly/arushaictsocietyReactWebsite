e52fbf70a44fc49c07953b700332d3c6673a7121
hijizaly <hijizal@gmail.com>
2023-02-10 21:56:29 +0300

    build_done

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir:'finalBuild',
+    outDir:'../finalBuild',
     chunkSizeWarningLimit: 600,
     rollupOptions: {
       output:{
